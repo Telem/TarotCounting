54481d1383c1e87b8641266e19721d2bf1b7dd91
Mathieu Sivade <mathieu.sivade@imgtec.com>
2014-11-05 15:20:09 +0000

    stats ui: make sections headers into links

--- common.js
+++ common.js
@@ -2,14 +2,15 @@
 $(function(){
 	$('.tab').each(function(i, elem) {
 		var groupName = this.dataset.groupname;
-		var $tabSummaryLink = $('<div class="pseudolink">'+groupName+'</div>');
+		var groupID = encodeURIComponent(groupName);
+		var $tabSummaryLink = $('<a href="#'+groupID+'" class="pseudolink">'+groupName+'</a>');
 		$tabSummaryLink.click(function(){
 			$('html, body').animate({
 				scrollTop: $('.tab[data-groupname="'+groupName+'"]').offset().top
 			}, 500);
 		});
 		$('.tab-summary').append($tabSummaryLink);
-		$('<h3>'+groupName+'</h3>').insertBefore(this);
+		$('<h3><a id="'+groupID+'">'+groupName+'</a></h3>').prependTo(this);
 	});
 });
 
